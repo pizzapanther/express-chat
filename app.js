619dc3bf16f714645ab722eb4eeafc915b942cb2
Paul Bailey <paul.m.bailey@gmail.com>
2018-05-25 00:29:20 +0000

    setup

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+var app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  noCache: true
+});
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/socket-io',
+  express.static('node_modules/socket.io-client/dist'));
+
+app.get('/', function (request, response) {
+  response.render('chat.html');
+});
+
+io.on('connection', function(client){
+  console.log('CONNECTED');
+  
+  client.on('join-room', function(room){
+    client.join(room, function() {
+      console.log('ROOMS', client.rooms);
+      io.to(room).emit('chat-msg', '**new user joined**');
+    });
+    
+    client.on('incoming', function(msg){
+      console.log(msg);
+      io.to(msg.room).emit('chat-msg', msg.msg);
+    });
+  });
+  
+  client.on('disconnect', function () {
+    console.log('EXITED');
+  });
+});
+
+http.listen(8080, function () {
+  console.log('Listening on port 8080');
+});
